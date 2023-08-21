82c6cfa3a730ffb062ca359ce7f454c2c4439b81
ZhangGuangZe <zhanggz1997@126.com>
2023-08-21 10:52:05 +0800

    build: 支持文件构建

--- src/entry.ts
+++ src/entry.ts
@@ -1,15 +1,15 @@
 import { App } from "vue";
-import MyButton from "./button";
+import ViteButton from "./button";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
 
 // 导出单独组件
-export { MyButton, SFCButton, JSXButton };
+export { ViteButton, SFCButton, JSXButton };
 
 // 编写一个插件，实现一个install方法
 export default {
   install(app: App): void {
-    app.component(MyButton.name, MyButton);
+    app.component(ViteButton.name, ViteButton);
     app.component(SFCButton.name, SFCButton);
     app.component(JSXButton.name, JSXButton);
   },
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,26 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "ViteUI",
+      fileName: "vite-ui",
+      // 导出模块格式
+      formats: ["es", "umd","iife"],
+    },
+  },
 });
