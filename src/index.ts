5077b391b2925dcef433012fb768cb31c41856a0
ZhangGuangZe <zhanggz1997@126.com>
2023-08-21 10:29:49 +0800

    feat: 使用 Vite 从零搭建一个支持 SFC 和 JSX 语法的 Vue 开发环境

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import ViteButton from "./button";
+import SFCButton from "./SFCButton.vue";
+import JSXButton from "./JSXButton";
+
+createApp(JSXButton).mount("#app");
